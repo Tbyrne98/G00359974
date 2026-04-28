8f612824985d1f9385fa83f970609d02498f8690
Tbyrne98 <G00359974@atu.ie>
2026-04-28 18:42:14 +0000

    Logged on, did a bit of work on the home page, ran into issues with the Favourites icon. Don't have time right now to fix, will research and come back to it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
+  
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ heart });
+  }
+}
+
+@Icon({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  imports: [IonButton, IonIcon],
+})
+export class IconComponent {
+  constructor() {
+    addIcons({ heart });
+  }
 }
