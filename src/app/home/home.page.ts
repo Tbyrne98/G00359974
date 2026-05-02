7483f4ea5fe483672fe038df2c7b8180c8eb78b6
Tbyrne98 <G00359974@atu.ie>
2026-05-02 20:07:28 +0000

    Got my API and checked if it worked for the Today's Trending Movies which it did so I started following the exercise from week 11 for guidance. I followed the video until I tested using the JSON.stringify and I belive it worked as I checked using the console on the webpage but then I continued to follow the video so that the list would return on the home page. I don't have any errors in the Visual studio code app but once I run it and check the console on the home page I get a bunch of errors so I will need to revisit this tomorrow. I belive I have all my imports as I don't get errors for them so it must be an issue with my methods or variable@

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
-
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MyHttp } from '../services/my-http';
+import { HttpOptions } from '@capacitor/core';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
+  imports: [IonHeader, IonToolbar, CommonModule, IonTitle, IonContent, IonButton, RouterLink, IonInput, FormsModule, IonList, IonItem, IonLabel],
 })
 export class HomePage {
-  constructor() {}
+
+  options: HttpOptions = {
+    url: "https://api.themoviedb.org/3/trending/movie/day?api_key=c185f5ab98624d8025f52afe61f303f9"
+  }
+
+  TodaysTrendingMovies:any = [];
+
+  MovieName: string = ""
+  constructor(private mh:MyHttp) {}
+
+  ngOnInit() {
+    this.getTodaysTrendingMovies();
+  }
+  
+  async getTodaysTrendingMovies() {
+    var result = await this.mh.get(this.options)
+    this.TodaysTrendingMovies = result.data
+    console.log(this.TodaysTrendingMovies)
+  }
 }
