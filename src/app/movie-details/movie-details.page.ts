bdff69f276e7e25f02a2e7d2259403e4cc0e2da0
Tbyrne98 <G00359974@atu.ie>
2026-05-03 14:04:38 +0000

    I logged back on and followed the second half of the Week 11 exercise video however this was a misktake as I have added the information from the Trending today movie details to the movie details page when that should have been on the home page and the movie details page has it's API so I need to undo what I've done and then do it correctly

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonList } from '@ionic/angular/standalone';
 import { Data } from '../services/data';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonItem, IonList]
 })
 export class MovieDetailsPage implements OnInit {
 
-movie: any;
+movie: any = {
+  value:""
+};
+
+movieDetails: any = [];
 
   constructor(private md: Data) { }
 
@@ -22,7 +26,10 @@ movie: any;
 
   async getMovieFromStorage() {
     this.movie = await this.md.get("movie")
-    console.log(this.movie)
+    for (const details in this.movie) {
+      this.movieDetails.push(details)
+    }
+    console.log(this.movieDetails)
   }
 
 }
